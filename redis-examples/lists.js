b0709731c03fb07907cb13faa3eaac6278f37134
AT <hypothetical.andrei@gmail.com>
2023-05-28 11:22:50 +0300

    .

---
+++ redis-examples/lists.js
@@ -0,0 +1,26 @@
+import redis from 'redis'
+import { v4 as uuid } from 'uuid'
+
+try {
+  const client = redis.createClient()
+  await client.connect()
+  await client.del('asset:1')
+  await client.lPush('asset:1', 'a')
+  await client.lPush('asset:1', 'b')
+  await client.lPush('asset:1', 'c')
+  await client.lPush('asset:1', 'd')
+  await client.lPush('asset:1', 'e')
+  let result = await client.lPop('asset:1')
+  // console.log(result)
+  result = await client.lRange('asset:1', 2, 4)
+  // console.log(result)
+  const id = uuid()
+  await client.lSet('asset:1', 2, id)
+  await client.lRem('asset:1', 1, id)
+  const len = await client.lLen('asset:1')
+  const remaining = await client.lRange('asset:1', 0, len)
+  console.log(remaining)
+  await client.quit()
+} catch (error) {
+  console.warn(error)  
+}
